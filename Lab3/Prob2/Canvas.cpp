9cb9f6bd60f4c2919341fe9eaa92f3ddd7a685b8
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-03-12 16:05:27 +0200

    Add files via upload

---
+++ Lab3/Prob2/Canvas.cpp
@@ -0,0 +1,51 @@
+#include "Canvas.h"
+#include <iostream>
+#include <cstdarg>
+using namespace std;
+
+Canvas::Canvas(int lines, int columns)
+{
+	this->height = lines;
+	this->length = columns;
+	this->clear();
+
+}
+
+void Canvas::set_pixel(int x, int y, char value)
+{
+	array[x][y] = value;
+}
+
+void Canvas::set_pixels(int count, ...)
+{
+	va_list args;
+	va_start(args, count);
+
+	for (int i = 0; i < count; i++) {
+		int x = va_arg(args, int);     
+		int y = va_arg(args, int);     
+		int val = va_arg(args, int);   
+
+		if (x >= 0 && x <height && y >= 0 && y <length) {
+			array[x][y] = val;
+		}
+	}
+
+	va_end(args);
+}
+
+void Canvas::clear()
+{
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < length; j++)
+			array[i][j] = '#';
+}
+
+void Canvas::print() const
+{
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < length; j++)
+			cout << array[i][j] << " ";
+		cout << endl;
+	}
+}
