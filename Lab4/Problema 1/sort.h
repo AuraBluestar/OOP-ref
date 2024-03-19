325bea3cfcfcc15e224a21d4797df867b15d34db
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-03-19 20:01:53 +0200

    Update sort.h

--- Lab4/Problema 1/sort.h
+++ Lab4/Problema 1/sort.h
@@ -22,18 +22,3 @@ public:
     int GetElementsCount();
     int GetElementFromIndex(int index);
 };
-/*add several constructors that will allow the following:
-
-1. create the list that needs to be sorted out of random values within a specific interval (min, max). 
-The constructor will receive 3 parameters (the number of elements in the list, minimum value, maximum value).
-
-2. create the list that needs to be sorted from an initialization list
-
-3. create the list that needs to be sorted from an existing vector 
-(the constructor will have two parameters - one being the vector, the other one being the number of elements from the vector)
-
-4. create the list that needs to be sorted using variadic parameters (use va_args for this)
-
-5. create the list that needs to be sorted from a string (e.g. "10,40,100,5,70" -> 
-each number is separated from the rest of the number with a comma). 
-It is assumed that the string is correctly written (no space, only numerical characters and commas) */
