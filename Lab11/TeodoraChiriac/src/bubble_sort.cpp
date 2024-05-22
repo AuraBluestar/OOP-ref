84942587586f6e2a34058ea892ed38db8636afd2
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-05-22 13:08:01 +0300

    Update bubble_sort.cpp

--- Lab11/TeodoraChiriac/src/bubble_sort.cpp
+++ Lab11/TeodoraChiriac/src/bubble_sort.cpp
@@ -5,7 +5,7 @@
 template<typename T>
 void bubbleSort(std::vector<T>& arr) {
     int n = arr.size();
-    for (int i = 0; i < n - 1; ++i) {
+    for ( i = 1; i < n - 1; ++i) {
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] < arr[j + 1]) { 
                 arr[j] = arr[j + 1];   
@@ -15,7 +15,7 @@ void bubbleSort(std::vector<T>& arr) {
 }
 
 int main() {
-    std::vector<int> vec = { 4, 3, 2, 1 };
+    std::vector<int> vec = { 5, 10, 2, 1 };
     bubbleSort(vec);
     for (int v : vec) {
         std::cout << v << " ";
