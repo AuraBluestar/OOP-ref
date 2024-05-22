a36f4d4facf9b06a4ff4a2100c430ceb47c14e55
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-05-22 13:05:44 +0300

    Update bubble_sort_test.cpp

--- Lab11/TeodoraChiriac/bubble_sort_test.cpp
+++ Lab11/TeodoraChiriac/bubble_sort_test.cpp
@@ -1,41 +1,45 @@
-//Onisor Maria-Patricia
-#include <iostream>
+//Onisor Maria Patricia
+#include <gtest/gtest.h>
 #include <vector>
-
+#include "bubble_sort_cpp"
 template<typename T>
 void bubbleSort(std::vector<T>& arr) {
     int n = arr.size();
     for (int i = 0; i < n - 1; ++i) {
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] < arr[j + 1]) { 
-                arr[j] = arr[j + 1];   
+                std::swap(arr[j], arr[j + 1]); 
             }
         }
     }
 }
 
-int main() {
+TEST(BubbleSortTest, BasicAssertions) {
     std::vector<int> vec = { 4, 3, 2, 1 };
     bubbleSort(vec);
+    EXPECT_EQ(vec, (std::vector<int>{ 4, 3, 2, 1 }));
+}
 
-    bool sorted = true;
-    for (size_t i = 1; i < vec.size(); ++i) {
-        if (vec[i - 1] > vec[i]) {
-            sorted = false;
-            break;
-        }
-    }
+TEST(BubbleSortTest, AlreadySorted) {
+    std::vector<int> vec = { 1, 2, 3, 4 };
+    bubbleSort(vec);
+    EXPECT_EQ(vec, (std::vector<int>{ 4, 3, 2, 1 }));
+}
 
-    if (!sorted) {
-        std::cout << "Sorting algorithm failed." << std::endl;
-    }
-    else {
-        std::cout << "Sorting algorithm succeeded." << std::endl;
-    }
+TEST(BubbleSortTest, ReverseSorted) {
+    std::vector<int> vec = { 4, 3, 2, 1 };
+    bubbleSort(vec);
+    EXPECT_EQ(vec, (std::vector<int>{ 4, 3, 2, 1 }));
+}
 
-    for (int v : vec) {
-        std::cout << v << " ";
-    }
-    std::cout << std::endl;
-    return 0;
+TEST(BubbleSortTest, AllSameElements) {
+    std::vector<int> vec = { 1, 1, 1, 1 };
+    bubbleSort(vec);
+    EXPECT_EQ(vec, (std::vector<int>{ 1, 1, 1, 1 }));
+}
+
+TEST(BubbleSortTest, EmptyVector) {
+    std::vector<int> vec = {};
+    bubbleSort(vec);
+    EXPECT_EQ(vec, (std::vector<int>{}));
 }
